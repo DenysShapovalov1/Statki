ff53937fbc9ecee04281d374c2bf525363e45880
DenysShapovalov1 <sdd261007@gmail.com>
2024-11-16 21:05:11 +0100

    Update Player.cc

--- Player.cc
+++ Player.cc
@@ -33,9 +33,6 @@ void Player::clearConsole() {
 #endif
 }
 
-
-
-
 void Player::placeShips() {
     int sizes[4] = {1, 2, 3, 4}; 
     int counts[4] = {4, 3, 2, 1}; 
@@ -76,7 +73,6 @@ void Player::placeShips() {
             }
 
             while (!board.placeShip(x, y, sizes[i], names[i], horizontal)) {
-                std::cout << "Nie można umieścić statek. Spróbuj ponownie.\n";
                 while (true) {
                     std::cout << "Podaj współrzędne (x y): ";
                     std::cin >> x >> y;
@@ -111,14 +107,12 @@ void Player::placeShips() {
         }
     }
     clearConsole();
-    board.hideShips();
 }
-
 
 bool Player::shoot(Player& opponent) {
     int x, y;
     while (true) {
-        std::cout << nickname << ", wprowadź współrzędne do strzału (x y): ";
+        std::cout << nickname << ", podaj współrzędne do strzału (x y): ";
         std::cin >> x >> y;
 
         if (std::cin.fail() || x < 0 || x > 9 || y < 0 || y > 9) {
@@ -128,24 +122,29 @@ bool Player::shoot(Player& opponent) {
             continue; 
         }
 
-        
-        if (opponent.board.shoot(x, y)) {
-            std::cout << "Trafiłeś!\n";
-            displayBoards(opponent);
-            if (opponent.allShipsSunk()) {
+        if (!opponent.board.shotHistory[x][y]) { 
+            if (opponent.board.shoot(x, y)) {
+                std::cout << "Trafiłeś!\n";
+                if (opponent.allShipsSunk()) {
                 std::cout << opponent.nickname << " wszystkie statki zostały zatopione!\n";
                 return false; 
             }
+            } else {
+                std::cout << "Nie Trafiłęś!\n";
+                break; 
+            }
         } else {
-            std::cout << "Nie trafiłeś.\n";
-            clearConsole();
-            break; 
+            std::cout << "Już strzelałeś w to miejsce! Spróbuj jeszcze raz.\n";
+            continue; 
         }
 
         clearConsole();
         displayBoards(opponent);
-        
     }
+    clearConsole();
+    std::cout << "Czy następny gracz jest gotowy? Wpisz dowolny klawisz, aby kontynuować...\n";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
     return true; 
 }
 
@@ -157,23 +156,14 @@ std::string Player::getNickname() const {
     return nickname;
 }
 
-
-
-
 void Player::playTwoPlayers(int& player1Wins, int& player2Wins) {
-    std::string player1Name, player2Name;
-    std::cout << "Podaj imię gracza 1: ";
-    std::cin >> player1Name;
+    std::string player1Name = getPlayerNickname(1);
+    std::string player2Name = getPlayerNickname(2);
 
-    while (true) {
-        std::cout << "Podaj imię gracza 2: ";
-        std::cin >> player2Name;
-
-        if (player1Name == player2Name) {
-            std::cout << "Nie możesz używać takiego samego imienia, wybierz coś innego! \n";
-        } else {
-            break;
-        }
+    
+    while (player1Name == player2Name) {
+        std::cout << "Imiona graczy nie mogą być takie same! Wybierz inne imię dla gracza 2.\n";
+        player2Name = getPlayerNickname(2); 
     }
 
     Player player1(player1Name);
@@ -200,7 +190,6 @@ void Player::playTwoPlayers(int& player1Wins, int& player2Wins) {
 
     time_t endTime = time(0); 
     double duration = difftime(endTime, startTime); 
-
     if (player1.allShipsSunk()) {
         std::cout << player2.getNickname() << " wygrał!\n";
         player2Wins++; 
@@ -208,7 +197,17 @@ void Player::playTwoPlayers(int& player1Wins, int& player2Wins) {
         std::cout << player1.getNickname() << " wygrał!\n";
         player1Wins++; 
     }
-
     std::cout << "Czas trwania gry: " << duration << " sekund.\n";
     std::cout << "Wygrane - Gracz 1: " << player1Wins << ", Gracz 2: " << player2Wins << "\n";
 }
+
+std::string Player::getPlayerNickname(int playerNumber) {
+    std::string playerName;
+    
+    
+    std::cout << "Podaj imię gracza " << playerNumber << ": ";
+    std::cin >> playerName;
+
+    return playerName;
+}
+
