42d60a3259106399c7ee266f1aacce560edefeb3
DenysShapovalov1 <sdd261007@gmail.com>
2024-11-16 21:05:44 +0100

    Update Ship.h

--- Ship.h
+++ Ship.h
@@ -3,16 +3,14 @@
 
 #include <string>
 
-
-
 class Ship {
 public:
     int x, y; 
     int size; 
     std::string name;
     bool horizontal; 
-
     Ship(int x, int y, int size, const std::string& name, bool horizontal);
+    Ship() : x(0), y(0), size(0), name(""), horizontal(true) {}
 };
 
 #endif 
